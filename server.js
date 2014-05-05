e6a2f42f9982b3df58ed3fc3963d2ee7499d3f01
Lily Baker <lilybaker90@mac.com>
2014-05-05 18:22:51 +0100

    Fixed server.js

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-var express = require('express');
-var server = express();
-
 server.use(express.static('public'));
 server.listen(3000)
+
+var express = require("express");
+var logfmt = require("logfmt");
+var server = express();
+
+server.use(logfmt.requestLogger());
+server.use(express.static('public'));
+
+var port = Number(process.env.PORT || 3000);
+server.listen(port, function() {
+  console.log("Listening on " + port);
+});
