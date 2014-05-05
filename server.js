5a56368593a915add029e33cd69cc23315cb348b
Lily Baker <lilybaker90@mac.com>
2014-05-05 18:45:01 +0100

    Fixed up server.js

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-server.use(express.static('public'));
-server.listen(3000)
-
 var express = require("express");
-var logfmt = require("logfmt");
-var server = express();
-
-server.use(logfmt.requestLogger());
-server.use(express.static('public'));
-
-var port = Number(process.env.PORT || 3000);
-server.listen(port, function() {
-  console.log("Listening on " + port);
-});
+var app = express();
+var port = process.env.PORT || 3000;
+ 
+app.get('/', function(request, response) {
+    response.sendfile(__dirname + '/public/index.html');
+}).configure(function() {
+    app.use('/', express.static(__dirname + '/public'));
+}).listen(port);
